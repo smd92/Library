8f1883735c9384267e47f4115a377bde0d88d4a9
sassan <sassan.marouf@gmail.com>
2020-11-20 19:04:43 +0100

    .

--- script.js
+++ script.js
@@ -18,30 +18,45 @@ Fish.prototype.info = function() {
   }
 
 function addFishToCatches(sp, s, b, m) {
-	myCatches.push(new Fish(sp, s, b, m));
+  let newFish = new Fish(sp, s, b, m);
+  myCatches.push(newFish);
+  return newFish;
 }
 
 // --- end of objects part
 
 const grid = document.querySelector("#grid");
-const addFish = document.querySelector("addFish");
+const addFish = document.querySelector("#addFish");
 
 //initial grid
 if (myCatches.length < 1) {
   window.onload = adjustGrid(4, 4);
 }
 else {
-  window.onload = adjustGrid(myCatches.length, 3);
+  window.onload = adjustGrid(myCatches.length + 1, 3);
 }
 
+//adjust values of css variables
 function adjustGrid(columns, rows) {
-	
-  //adjust values of css variables
   grid.style.setProperty("--columns", columns);
   grid.style.setProperty("--rows", rows);
 }
 
 //open form for user input
 addFish.addEventListener(("click"), () => {
-
+  
 })
+
+
+let testOne = addFishToCatches("Bachforelle", "35", "Trockenfliege", "Fliegenfischen");
+let testTwo = addFishToCatches("Döbel", "50", "Nymphe", "Fliegenfischen");
+
+//display myCatches items on page
+function displayCatches() {
+
+  myCatches.forEach((item) => {
+    let newTag = document.createElement("p");
+    newTag.textContent = item.species;
+    grid.insertBefore(newTag, addFish);
+  })
+}
