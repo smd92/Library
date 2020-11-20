ef5cfcdab24b861570db8c219dfa2d6c1d0f9dd6
sassan <sassan.marouf@gmail.com>
2020-11-20 11:07:58 +0100

    .

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-let myLibrary = [];
+// --- objects part ---
+
+let myCatches = [];
 
 //constructor function for new book objects
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Fish(species, size, bait, method) {
+    this.species = species
+    this.size = size + "cm";
+    this.bait = bait
+    this.method = method
 }
 
 //info about the book
-Book.prototype.info = function() {
+Fish.prototype.info = function() {
     let infoArr = [];
-    infoArr.push(this.title, this.author, this.pages, this.read);
+    infoArr.push(this.species, this.size, this.bait, this.method);
     return infoArr.join(", ");
   }
 
-function addBookToLibrary(t, a, p, r) {
-	myLibrary.push(new Book(t, a, p, r));
+function addFishToCatches(sp, s, b, m) {
+	myCatches.push(new Fish(sp, s, b, m));
 }
+
+// --- end of objects part
+
+const grid = document.querySelector("#grid");
+const addFish = document.querySelector("addFish");
+
+//initial grid
+if (myCatches.length < 1) {
+  window.onload = adjustGrid(4, 4);
+}
+else {
+  window.onload = adjustGrid(myCatches.length, 3);
+}
+
+function adjustGrid(columns, rows) {
+	
+  //adjust values of css variables
+  grid.style.setProperty("--columns", columns);
+  grid.style.setProperty("--rows", rows);
+}
+
+//open form for user input
+addFish.addEventListener(("click"), () => {
+
+})
