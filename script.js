69ac846ce3536c3ebfdd51a6efa012f5a43521be
sassan <sassan.marouf@gmail.com>
2020-12-06 12:17:57 +0100

    .

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ if (myCatches.length < 1) {
 }
 else {
   window.onload = adjustGrid(myCatches.length + 1, 3);
+  displayCatches();
 }
 
 //adjust values of css variables
@@ -81,76 +82,73 @@ function closeModal(modal) {
 let testOne = addFishToCatches("Bachforelle", "35", "Trockenfliege", "Mangfall", "Fliegenfischen", "16.04.2020");
 let testTwo = addFishToCatches("Döbel", "50", "Nymphe", "Inn", "Fliegenfischen", "22.06.2020");
 let testThree = addFishToCatches("Hecht", "82", "Gummifisch", "Tegernsee", "Schleppfischen", "15.10.2020");
-let testFour = addFishToCatches("Hecht", "82", "Gummifisch", "Tegernsee", "Schleppfischen", "15.10.2020");
 //      END OF TEST
 
 //display myCatches items on page
-function displayCatches() {
+function displayCatches(item) {
 
-  myCatches.forEach((item) => {
-    //create new tile
-    let newTile = document.createElement("div");
-    newTile.classList.add("fish", "action");
+  //create new tile
+  let newTile = document.createElement("div");
+  newTile.classList.add("fish", "action");
 
-    //create edit and remove button
-    let edRe = document.createElement("div");
-    edRe.className = "edRe";
+  //create edit and remove button
+  let edRe = document.createElement("div");
+  edRe.className = "edRe";
 
-    let edit = document.createElement("p");
-    edit.className = "edit";
-    edit.textContent = "Edit";
-    edRe.appendChild(edit);
+  let edit = document.createElement("p");
+  edit.className = "edit";
+  edit.textContent = "Edit";
+  edRe.appendChild(edit);
 
-    let remove = document.createElement("p");
-    remove.className = "remove";
-    remove.textContent = "Remove";
-    edRe.appendChild(remove);
+  let remove = document.createElement("p");
+  remove.className = "remove";
+  remove.textContent = "Remove";
+  edRe.appendChild(remove);
     
-    //create content-body -> species, size
-    let specSize = document.createElement("div");
-    specSize.className = "specSize";
+  //create content-body -> species, size
+  let specSize = document.createElement("div");
+  specSize.className = "specSize";
 
-    let species = document.createElement("p");
-    species.className = "species";
-    species.textContent = item.species;
-    specSize.appendChild(species);
+  let species = document.createElement("p");
+  species.className = "species";
+  species.textContent = item.species;
+  specSize.appendChild(species);
 
-    let size = document.createElement("p");
-    size.className = "size";
-    size.textContent = item.size;
-    specSize.appendChild(size);
+  let size = document.createElement("p");
+  size.className = "size";
+  size.textContent = item.size;
+  specSize.appendChild(size);
 
-    //create info row -> bait, method
-    let baitMet = document.createElement("div");
-    baitMet.className = "baitMet";
+  //create info row -> bait, method
+  let baitMet = document.createElement("div");
+  baitMet.className = "baitMet";
     
-    let bait = document.createElement("p");
-    bait.className = "bait";
-    bait.textContent = item.bait;
-    baitMet.appendChild(bait);
+  let bait = document.createElement("p");
+  bait.className = "bait";
+  bait.textContent = item.bait;
+  baitMet.appendChild(bait);
 
-    let method = document.createElement("p");
-    method.className = "method";
-    method.textContent = item.method;
-    baitMet.appendChild(method);
+  let method = document.createElement("p");
+  method.className = "method";
+  method.textContent = item.method;
+  baitMet.appendChild(method);
     
-    //create info row -> location
-    let location = document.createElement("p");
-    location.className = "location";
-    location.textContent = item.location;
+  //create info row -> location
+  let location = document.createElement("p");
+  location.className = "location";
+  location.textContent = item.location;
 
-    //create info row -> catch date
-    let date = document.createElement("p");
-    date.className = "date";
-    date.textContent = item.date;
+  //create info row -> catch date
+  let date = document.createElement("p");
+  date.className = "date";
+  date.textContent = item.date;
 
-    newTile.appendChild(edRe);
-    newTile.appendChild(specSize);
-    newTile.appendChild(baitMet);
-    newTile.appendChild(location);
-    newTile.appendChild(date);
-    grid.insertBefore(newTile, addFishButton);
-  })
+  newTile.appendChild(edRe);
+  newTile.appendChild(specSize);
+  newTile.appendChild(baitMet);
+  newTile.appendChild(location);
+  newTile.appendChild(date);
+  grid.insertBefore(newTile, addFishButton);
 }
 
 //grab and process form data
@@ -176,9 +174,17 @@ submit.addEventListener(("click"), () => {
   gerDateArr.push(dateArr[6], dateArr[7], ".", dateArr[4], dateArr[5], ".", dateArr[0], dateArr[1], dateArr[2], dateArr[3]);
   let gerDate = gerDateArr.join("");
   
-  addFishToCatches(species, size, bait, method, location, gerDate);
-  displayCatches();
+  addFishToCatches(species, size, bait, location, method, gerDate);
+
+  //display the newly created object
+  let indexNew = myCatches.length - 1;
+  displayCatches(myCatches[indexNew]);
 
   const modal = closeModalButton.closest(".modal");
   closeModal(modal);
+})
+
+//display array items by default
+myCatches.forEach((item) => {
+  displayCatches(item);
 })
